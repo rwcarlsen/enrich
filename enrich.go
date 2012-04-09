d634525069eb7e896c4e86f02047e33d778a2d65
Robert Carlsen <rwcarlsen@gmail.com>
2012-04-09 08:51:17 -0500

    more work

--- enrich.go
+++ enrich.go
@@ -6,33 +6,58 @@ import (
   "math"
 )
 
+const (
+  kWhkg2GWdMT = 24000
+)
+
 func main() {
   m := &Matl{M:1}
 
   mine(m)
   convert(m)
   enrich(m, 0.035, 0.003)
+  dispose(m)
 
   fmt.Println(m)
   fmt.Println(m.ValPerKg())
+  fmt.Println(m.ValPerKWh(bu))
+}
+
+func onceThrough(m *Matl) {
+  mine(m)
+  convert(m)
+  enrich(m, 0.035, 0.003)
+  burn(m, 3)
+  dispose(m)
+
+  fmt.Println(m)
+  fmt.Println(m.ValPerKg())
+  fmt.Println(m.ValPerKWh())
 }
 
 type (
   Chemform string
-  Enrichment float64
-  Value float64
-  Mass float64
+  Enrichment float64 // mass frac 235
+  Burnup float64 // GWD/MTU
+  Value float64 // $
+  Energy float64 // kWh
+  Mass float64 // kg
 )
 
 type Matl struct {
   Form Chemform
-  Enrich Enrichment // mass frac 235
-  Val Value // $/kg
-  M Mass // kg
+  E Energy
+  Enrich Enrichment
+  Val Value
+  M Mass
 }
 
 func (m *Matl) ValPerKg() Value {
   return m.Val / Value(m.M)
+}
+
+func (m *Matl) ValPerKWh(bu Burnup) Value {
+  return m.Val / Value(m.E)
 }
 
 func mine(m *Matl) *Matl {
@@ -73,4 +98,20 @@ func potential(enrich Enrichment) float64 {
   return (1 - 2 * x) * math.Log((1 - x) / x)
 }
 
+func dispose(m *Matl) *Matl {
+  m.Val += 2000 * Value(m.M)
+  return m
+}
+
+func burn(m *Matl, nBatches int) *Matl {
+  // modify b1 formula to taste
+  b1 := Burnup(1000) * Burnup(m.Enrich)
+
+  // LRM discharge burnup
+  n := Burnup(nBatches)
+  bd := 2 * n / (n + 1) * b1
+
+  m.E = Energy(bd) * kWhkg2GWdMT * Energy(m.M)
+  return m
+}
 
